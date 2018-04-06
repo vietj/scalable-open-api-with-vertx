f0b57e13af571183536beecf9e939c226f8973a6
Julien Viet <julien@julienviet.com>
2018-04-06 22:51:19 +0200

    Modularize the operations

--- src/main/java/com/julienviet/scalableopenapis/OpenAPIServer.java
+++ src/main/java/com/julienviet/scalableopenapis/OpenAPIServer.java
@@ -7,6 +7,7 @@ import io.vertx.core.json.JsonArray;
 import io.vertx.core.json.JsonObject;
 import io.vertx.core.net.SelfSignedCertificate;
 import io.vertx.ext.web.Router;
+import io.vertx.ext.web.RoutingContext;
 import io.vertx.ext.web.api.RequestParameters;
 import io.vertx.ext.web.api.contract.openapi3.OpenAPI3RouterFactory;
 import io.vertx.core.http.HttpServerOptions;
@@ -29,32 +30,17 @@ public class OpenAPIServer extends AbstractVerticle {
 
     @Override
     public void start(final Future<Void> bootstrapFuture) throws Exception {
+
         // Start creating the router factory from openapi.yaml
         OpenAPI3RouterFactory.create(vertx, "openapi.yaml", ar -> {
+
             // The router factory instantiation could fail
             if (ar.succeeded()) {
                 OpenAPI3RouterFactory factory = ar.result();
 
                 // Now you can use the factory to mount map endpoints to Vert.x handlers
-                factory.addHandlerByOperationId("listUsers", (routingContext) -> {
-                    RequestParameters params = routingContext.get("parsedParameters");
-                    // Do what you want to retrieve the users with limit
-                    routingContext
-                            .response()
-                            .setStatusCode(200)
-                            .end(users.encode());
-                });
-                factory.addHandlerByOperationId("addUser", (routingContext) -> {
-                    RequestParameters params = routingContext.get("parsedParameters");
-                    String userId = "" + users.size();
-                    JsonObject user = params.body().getJsonObject();
-                    users.add(user);
-                    // Do what you want to add user to your persistence layer
-                    routingContext
-                            .response()
-                            .setStatusCode(200)
-                            .end(userId);
-                });
+                factory.addHandlerByOperationId("listUsers", this::listUsers);
+                factory.addHandlerByOperationId("addUser", this::addUser);
 
                 // Generate the certificate for https
                 SelfSignedCertificate cert = SelfSignedCertificate.create();
@@ -70,11 +56,31 @@ public class OpenAPIServer extends AbstractVerticle {
                     .listen();
 
                 bootstrapFuture.complete();
-
             } else {
                 bootstrapFuture.fail(ar.cause());
             }
         });
+    }
+
+    private void listUsers(RoutingContext routingContext) {
+        RequestParameters params = routingContext.get("parsedParameters");
+        // Do what you want to retrieve the users with limit
+        routingContext
+            .response()
+            .setStatusCode(200)
+            .end(users.encode());
+    }
+
+    private void addUser(RoutingContext routingContext) {
+        RequestParameters params = routingContext.get("parsedParameters");
+        String userId = "" + users.size();
+        JsonObject user = params.body().getJsonObject();
+        users.add(user);
+        // Do what you want to add user to your persistence layer
+        routingContext
+            .response()
+            .setStatusCode(200)
+            .end(userId);
     }
 
 }
