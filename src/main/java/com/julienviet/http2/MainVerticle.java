905b803fb66887a7f92dca5262d14bedb5beddfa
Julien Viet <julien@julienviet.com>
2018-04-06 21:15:17 +0200

    SSL support

--- src/main/java/com/julienviet/http2/MainVerticle.java
+++ src/main/java/com/julienviet/http2/MainVerticle.java
@@ -5,6 +5,7 @@ import io.vertx.core.Future;
 import io.vertx.core.Vertx;
 import io.vertx.core.json.JsonArray;
 import io.vertx.core.json.JsonObject;
+import io.vertx.core.net.SelfSignedCertificate;
 import io.vertx.ext.web.Router;
 import io.vertx.ext.web.api.RequestParameters;
 import io.vertx.ext.web.api.contract.openapi3.OpenAPI3RouterFactory;
@@ -20,7 +21,14 @@ public class MainVerticle extends AbstractVerticle {
         vertx.deployVerticle(new MainVerticle());
     }
 
-    private JsonArray users = new JsonArray();
+    private JsonArray users = new JsonArray()
+        .add(new JsonObject()
+            .put("firstName", "Julien")
+            .put("lastName", "Viet"))
+        .add(new JsonObject()
+            .put("firstName", "Francesco")
+            .put("lastName", "Guardiani")
+        );
 
     @Override
     public void start(final Future<Void> bootstrapFuture) throws Exception {
@@ -51,11 +59,18 @@ public class MainVerticle extends AbstractVerticle {
                             .end(userId);
                 });
 
+                // Generate the certificate for https
+                SelfSignedCertificate cert = SelfSignedCertificate.create();
+
                 // When you finish to map endpoints you can get the router with factory.getRouter()
                 Router router = factory.getRouter();
-                vertx.createHttpServer()
-                        .requestHandler(router::accept)
-                        .listen(8080);
+                vertx.createHttpServer(new HttpServerOptions()
+                    .setSsl(true)
+                    .setUseAlpn(true)
+                    .setKeyCertOptions(cert.keyCertOptions())
+                    .setPort(8443))
+                    .requestHandler(router::accept)
+                    .listen();
 
                 bootstrapFuture.complete();
 
--- src/test/java/com/julienviet/http2/UsersAPITest.java
+++ src/test/java/com/julienviet/http2/UsersAPITest.java
@@ -1,12 +1,14 @@
 package com.julienviet.http2;
 
 import io.vertx.core.Vertx;
+import io.vertx.core.http.HttpVersion;
 import io.vertx.core.json.JsonArray;
 import io.vertx.core.json.JsonObject;
 import io.vertx.ext.unit.Async;
 import io.vertx.ext.unit.TestContext;
 import io.vertx.ext.unit.junit.VertxUnitRunner;
 import io.vertx.ext.web.client.WebClient;
+import io.vertx.ext.web.client.WebClientOptions;
 import io.vertx.ext.web.codec.BodyCodec;
 import org.junit.After;
 import org.junit.Before;
@@ -23,27 +25,51 @@ public class UsersAPITest {
     public void before(TestContext ctx) {
         vertx = Vertx.vertx();
         vertx.deployVerticle(new MainVerticle(), ctx.asyncAssertSuccess());
-        client = WebClient.create(vertx);
+        client = WebClient.create(vertx, new WebClientOptions()
+            .setSsl(true)
+            .setTrustAll(true)
+            .setUseAlpn(true)
+            .setProtocolVersion(HttpVersion.HTTP_2));
     }
 
     @After
     public void after(TestContext ctx) {
         vertx.close(ctx.asyncAssertSuccess());
     }
+
     @Test
-    public void testUsers(TestContext ctx) {
+    public void testListUser(TestContext ctx) {
         Async async = ctx.async();
-        JsonObject expectedUser = new JsonObject().put("firstName", "Julien").put("lastName", "Viet");
-        client.post(8080, "localhost", "/users")
+        JsonArray expectedUsers = new JsonArray()
+            .add(new JsonObject().put("firstName", "Julien").put("lastName", "Viet"))
+            .add(new JsonObject().put("firstName", "Francesco").put("lastName", "Guardiani"));
+        client.get(8443, "localhost", "/users")
+            .as(BodyCodec.jsonArray())
+            .send(ctx.asyncAssertSuccess(resp2 -> {
+                ctx.assertEquals(200, resp2.statusCode());
+                ctx.assertEquals(expectedUsers, resp2.body());
+                async.complete();
+            }));
+    }
+
+    @Test
+    public void testAddUser(TestContext ctx) {
+        Async async = ctx.async();
+        JsonObject newUser = new JsonObject().put("firstName", "Dale").put("lastName", "Cooper");
+        JsonArray expectedUsers = new JsonArray()
+            .add(new JsonObject().put("firstName", "Julien").put("lastName", "Viet"))
+            .add(new JsonObject().put("firstName", "Francesco").put("lastName", "Guardiani"))
+            .add(newUser);
+        client.post(8443, "localhost", "/users")
             .as(BodyCodec.string())
-            .sendJsonObject(expectedUser, ctx.asyncAssertSuccess(resp1 -> {
+            .sendJsonObject(newUser, ctx.asyncAssertSuccess(resp1 -> {
                 ctx.assertEquals(200, resp1.statusCode());
-                ctx.assertEquals("0", resp1.body());
-                client.get(8080, "localhost", "/users")
+                ctx.assertEquals("2", resp1.body());
+                client.get(8443, "localhost", "/users")
                     .as(BodyCodec.jsonArray())
                     .send(ctx.asyncAssertSuccess(resp2 -> {
                         ctx.assertEquals(200, resp2.statusCode());
-                        ctx.assertEquals(new JsonArray().add(expectedUser), resp2.body());
+                        ctx.assertEquals(expectedUsers, resp2.body());
                         async.complete();
                     }));
             }));
