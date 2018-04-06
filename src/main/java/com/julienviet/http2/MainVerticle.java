3d12583c1e0fe4b3c542d1004546a087bf212b8a
francesco <francescoguard@gmail.com>
2018-04-06 13:30:34 +0200

    Added openapi stuff

--- src/main/java/com/julienviet/http2/MainVerticle.java
+++ src/main/java/com/julienviet/http2/MainVerticle.java
@@ -1,20 +1,55 @@
 package com.julienviet.http2;
 
 import io.vertx.core.AbstractVerticle;
+import io.vertx.core.Future;
+import io.vertx.core.json.JsonObject;
+import io.vertx.ext.web.Router;
+import io.vertx.ext.web.api.RequestParameters;
+import io.vertx.ext.web.api.contract.openapi3.OpenAPI3RouterFactory;
 import io.vertx.core.http.HttpServerOptions;
 
 public class MainVerticle extends AbstractVerticle {
 
     @Override
-    public void start() throws Exception {
+    public void start(final Future<Void> bootstrapFuture) throws Exception {
+        // Start creating the router factory from openapi.yaml
+        OpenAPI3RouterFactory.create(vertx, "openapi.yaml", ar -> {
+            // The router factory instantiation could fail
+            if (ar.succeeded()) {
+                OpenAPI3RouterFactory factory = ar.result();
 
-        vertx.createHttpServer().requestHandler(req -> {
-            req.response().end("Hello World");
-        }).listen(8080);
-        System.out.println("HTTP server started on port 8080");
+                // Now you can use the factory to mount map endpoints to Vert.x handlers
+                factory.addHandlerByOperationId("listUsers", (routingContext) -> {
+                    RequestParameters params = routingContext.get("parsedParameters");
+                    Integer limit = params.queryParameter("listUsers").getInteger();
+                    // Do what you want to retrieve the users with limit
+                    routingContext
+                            .response()
+                            .setStatusCode(200)
+                            .end(users);
+                });
+                factory.addHandlerByOperationId("addUser", (routingContext) -> {
+                    RequestParameters params = routingContext.get("parsedParameters");
+                    JsonObject user = params.body().getJsonObject();
+                    // Do what you want to add user to your persistence layer
+                    routingContext
+                            .response()
+                            .setStatusCode(200)
+                            .end(persistedUser);
+                });
 
-        long now = System.currentTimeMillis();
+                // When you finish to map endpoints you can get the router with factory.getRouter()
+                Router router = factory.getRouter();
+                vertx.createHttpServer()
+                        .requestHandler(router::accept)
+                        .listen(8080);
 
+                bootstrapFuture.complete();
+
+            } else {
+                bootstrapFuture.fail(ar.cause());
+            }
+        });
     }
 
 }
